4f88432456f8ffec9280f542ee2b65801e3070d1
apisarit <a.nirutsaprada@gmail.com>
2026-08-01 03:13:56 +0700

    Fix Supabase config field compatibility

--- auth-config.js
+++ auth-config.js
@@ -1,4 +1,6 @@
 window.CHANANYA_AUTH = Object.freeze({
+  url: 'https://qptxnrldzzinlcabudjv.supabase.co',
+  anonKey: 'sb_publishable_YCIY8LZeCfRgcSZYgmP6JA_r9OVbpcJ',
   supabaseUrl: 'https://qptxnrldzzinlcabudjv.supabase.co',
   publishableKey: 'sb_publishable_YCIY8LZeCfRgcSZYgmP6JA_r9OVbpcJ',
   redirectTo: 'https://chananya.netlify.app'
