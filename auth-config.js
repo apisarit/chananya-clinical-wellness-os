942ed4f8518dd85ca7c3bbdd00e0385954a73c65
apisarit <a.nirutsaprada@gmail.com>
2026-08-01 02:31:56 +0700

    Fix Supabase config compatibility aliases

--- auth-config.js
+++ auth-config.js
@@ -1,5 +1,8 @@
 window.CHANANYA_AUTH = Object.freeze({
   supabaseUrl: 'https://qptxnrldzzinlcabudjv.supabase.co',
+  url: 'https://qptxnrldzzinlcabudjv.supabase.co',
   publishableKey: 'sb_publishable_YCIY8LZeCfRgcSZYgmP6JA_r9OVbpcJ',
+  anonKey: 'sb_publishable_YCIY8LZeCfRgcSZYgmP6JA_r9OVbpcJ',
+  key: 'sb_publishable_YCIY8LZeCfRgcSZYgmP6JA_r9OVbpcJ',
   redirectTo: 'https://chananya.netlify.app'
 });
