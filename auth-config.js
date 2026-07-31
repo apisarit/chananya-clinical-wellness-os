66f90fb15f2e4759883d4d96363c77354f026daf
apisarit <a.nirutsaprada@gmail.com>
2026-07-31 23:59:37 +0700

    Configure Supabase Google authentication

---
+++ auth-config.js
@@ -0,0 +1,5 @@
+window.CHANANYA_AUTH = Object.freeze({
+  supabaseUrl: 'https://qptxnrldzzinlcabudjv.supabase.co',
+  publishableKey: 'sb_publishable_YCIY8LZeCfRgcSZYgmP6JA_r9OVbpcJ',
+  redirectTo: 'https://chananya.netlify.app'
+});
